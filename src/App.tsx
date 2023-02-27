8ffcb89753af9ddf58c68e4bdebe324fde2c1af3
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-27 23:41:18 +0100

    did the routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components';
+import Header from './Components/Header/Header';
+import HomeScreen from './Components/HomeScreen/HomeScreen';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div>
-      App
+      <Header/>
+      <HomeScreen/>
+      <Footer/>
     </div>
   );
 }
