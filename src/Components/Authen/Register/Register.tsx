ae98a722c7a70bd9438e226dcee9cdf44b25f899
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-28 16:26:15 +0100

    started the register page

--- src/Components/Authen/Register/Register.tsx
+++ src/Components/Authen/Register/Register.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Register = () => {
   return (
     <div >
-      Register Page
+      <Container>
+        <Wrapper>
+
+        </Wrapper>
+      </Container>
     </div>
   )
 }
 
 export default Register
+
+const Wrapper = styled.div``
+
+const Container = styled.div``
