17e7208dbeb212134d14523b17358202de3145fb
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-28 11:25:59 +0100

    added the Links

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import { Link} from 'react-router-dom'
+import styled from 'styled-components'
 
 const Header = () => {
   return (
     <div>
-        This is My header
+      <Container>
+        <Wrapper>
+          <Logo>
+            <Img/>
+          </Logo>
+
+          <NavLink >
+            <Nav to="">Save</Nav>
+            <Nav to ="">Invest</Nav>
+            <Nav to ="">Stories</Nav>
+            <Nav to="">FAQs</Nav>
+            <Nav to ="">Blog</Nav>
+            <Nav to="">E-Book</Nav>
+          </NavLink>
+        </Wrapper>
+      </Container>
     </div>
   )
 }
 
 export default Header
+
+const Nav = styled(Link)``
+
+const NavLink = styled.div``
+
+const Img = styled.img``
+
+const Logo = styled.div``
+
+const Wrapper = styled.div``
+
+const Container = styled.div``
